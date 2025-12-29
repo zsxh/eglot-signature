2c37bbd093adfe12c932e622f60b7c623237374a
zsxh <bnbvbchen@gmail.com>
2025-12-29 13:03:36 +0800

    eask init

---
+++ eglot-signature.el
@@ -0,0 +1,37 @@
+;;; eglot-signature.el --- Signature for eglot  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  zsxh
+
+;; Author: zsxh <bnbvbchen@gmail.com>
+;; Maintainer: zsxh <bnbvbchen@gmail.com>
+;; URL: https://github.com/zsxh/eglot-signature
+;; Version: 0.1
+;; Package-Requires: ((emacs "30.1"))
+;; Keywords: eglot signature tools
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Signature for eglot
+;;
+
+;;; Code:
+
+;; Happy coding! ;)
+
+(provide 'eglot-signature)
+;;; eglot-signature.el ends here
